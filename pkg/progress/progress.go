22c0bc1aa89083e00d8a0205301123c6404e6b59
Sergey Cherepiuk <s.cherepiuk@gmail.com>
2023-11-02 07:04:24 +0100

    #7: Displaying ETA (Estimated Time of Arrival) The ETA is updated by separate goroutine once a second

--- pkg/progress/progress.go
+++ pkg/progress/progress.go
@@ -4,26 +4,42 @@ import (
 	"fmt"
 	"os"
 	"strings"
+	"time"
 )
 
 func Display(ch <-chan int, size int) {
 	fmt.Fprintln(os.Stderr)
 
 	var bytesRead int
+
+	var start time.Time
+	var eta time.Duration
+	go func() {
+		for {
+			ratio := float64(size-bytesRead) / float64(bytesRead)
+			eta = time.Duration(ratio*time.Since(start).Seconds()) * time.Second
+			time.Sleep(time.Second)
+		}
+	}()
+
 	for n := range ch {
+		if start.IsZero() {
+			start = time.Now()
+		}
+
 		bytesRead += n
-		write(float32(bytesRead) / float32(size) * 100)
+		write(float32(bytesRead)/float32(size)*100, eta)
 	}
-	write(100.0)
+	write(100.0, 0)
 }
 
-func write(percent float32) {
-	fmt.Fprintf(os.Stderr, "\033[1A\033[K%s\n", format(percent, 50))
+func write(progress float32, eta time.Duration) {
+	fmt.Fprintf(os.Stderr, "\033[1A\033[K%s\n", format(progress, eta, 50))
 }
 
-func format(percent float32, length int) string {
+func format(progress float32, eta time.Duration, length int) string {
 	f := float32(100.0) / float32(length)
-	done := strings.Repeat("=", int(percent/f))
-	left := strings.Repeat(" ", length-int(percent/f))
-	return fmt.Sprintf("Progress: [%s%s] %6.2f%%", done, left, percent)
+	done := strings.Repeat("=", int(progress/f))
+	left := strings.Repeat(" ", length-int(progress/f))
+	return fmt.Sprintf("Progress: [%s%s] %6.2f%% | ETA: %v", done, left, progress, eta)
 }
